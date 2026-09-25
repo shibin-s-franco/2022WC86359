6d8b9c8e16b0258a8d499ab9ca7b1276b1ad2bf0
Shibin S Franco <shibinsfranco@gmail.com>
2026-09-25 07:16:00 +0530

    Initial Maven project for DevOps lab

---
+++ src/main/java/com/bits/devops/App.java
@@ -0,0 +1,12 @@
+package com.bits.devops;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("DevOps Continuous Integration Lab");
+        System.out.println("Name: Shibin S Franco");
+        System.out.println("BITS ID: 2022WC86359");
+        System.out.println("Java 21 Maven Build Successful");
+
+    }
+}
