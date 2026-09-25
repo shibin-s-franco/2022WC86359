80748bc0778db622c85ca496dfce7ffc523e25b5
Shibin S Franco <shibinsfranco@gmail.com>
2026-09-25 09:18:11 +0530

    Modified source code for CI

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -1,12 +1,13 @@
 package com.bits.devops;
 
 public class App {
+
     public static void main(String[] args) {
 
-        System.out.println("DevOps Continuous Integration Lab");
-        System.out.println("Name: Shibin S Franco");
-        System.out.println("BITS ID: 2022WC86359");
-        System.out.println("Java 21 Maven Build Successful");
+        System.out.println("DevOps Lab");
+        System.out.println("Student Name : Shibin S Franco");
+        System.out.println("BITS ID : 2022WC86359");
+        System.out.println("Continuous Integration Demonstration");
 
     }
 }
